6358e2e91e137277175063e1907f46e4166fdecf
Biriukov Egor <birykovegor@gmail.com>
2016-10-09 17:28:30 +0300

    mlet*

--- PartB/week2/hw.rkt
+++ PartB/week2/hw.rkt
@@ -117,7 +117,11 @@
 
 (define (ifaunit e1 e2 e3) (ifgreater (isaunit e1) (int 0) e2 e3))
 
-(define (mlet* lstlst e2) "CHANGE")
+(define (mlet* lstlst e2)
+  (if (null? lstlst)
+  e2
+  (let ([pair (car lstlst)])
+    (mlet (car pair) (cdr pair) (mlet* (cdr lstlst) e2)))))
 
 (define (ifeq e1 e2 e3 e4) "CHANGE")
 
--- PartB/week2/test.rkt
+++ PartB/week2/test.rkt
@@ -53,9 +53,13 @@
    (check-equal? (eval-exp (call (closure (list (cons "a" (int 1))) (fun "name" "z" (add (var "z") (var "a")))) (int 2))) (int 3) "call test")
    (check-exn exn:fail? (lambda () (eval-exp (call (fun "name" "z" (add (var "z") (var "a"))) (int 2)))))
 
-   ; ifaunit test
+   ;; ifaunit test
    (check-equal? (eval-exp (ifaunit (int 1) (int 2) (int 3))) (int 3) "ifaunit test")
    (check-equal? (eval-exp (ifaunit (aunit) (int 2) (int 3))) (int 2) "ifaunit test")
+
+   ;; mlet* test
+   (check-equal? (eval-exp (mlet* (list (cons "x" (int 10))) (var "x"))) (int 10) "mlet* test")
+   (check-equal? (eval-exp (mlet* (list (cons "x" (int 10)) (cons "y" (add (int 10) (var "x")))) (var "y"))) (int 20) "mlet* test")
    
    ))
 
