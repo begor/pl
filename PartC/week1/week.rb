715966919f0456332d4ecaa9d01f0dc9e2389bd1
Biryukov Egor <birykovegor@gmail.com>
2016-11-28 21:43:45 +0300

    Instance and class vars

--- PartC/week1/week.rb
+++ PartC/week1/week.rb
@@ -1,6 +1,6 @@
 class Hello
   def first_method
-    puts "Hello, world"   
+    puts "Hello, world"
   end
 end
 
@@ -8,7 +8,7 @@ h = Hello.new
 h.first_method
 
 class B
-  def m1 
+  def m1
     42
   end
 
@@ -16,3 +16,26 @@ class B
     x.abs + 2 * self.m1
   end
 end
+
+# Instance and Class state
+
+class C
+  Foo = "bar" # Class constant, don't mutate it
+
+  def initialize # Initializer
+    @foo = 0  # Instance's variable; private
+    @@bar = 0 # Class's variable, shared between instances; private
+  end
+
+  def m1 x
+    @foo += x   # Mutate @foo only for one concrete instace 
+    @@bar += 1  # Mutate @@bar across every instance of a class C
+  end
+
+  def foo # Kinda like accessor
+    @foo
+  end
+
+  def bar
+    @@bar
+  end
