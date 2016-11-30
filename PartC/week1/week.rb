f8611720f8f28dd9c13895a848842fb048fd5f40
Biryukov Egor <birykovegor@gmail.com>
2016-11-30 18:31:41 +0300

    Dynamic dispatch

--- PartC/week1/week.rb
+++ PartC/week1/week.rb
@@ -161,30 +161,75 @@ class Foo
   end
 end
 
+# Subclassing and dynamic dispatch
 
-# Subclassing
 class Point
-  attr_accessor :x, :y  # defines methods x, y, x=, y=
+  attr_accessor :x, :y
 
-  def initialize(x, y)
+  def initialize(x,y)
     @x = x
     @y = y
   end
-
-  def distanceFromOrigin
-    Math.sqrt(@x * @x + @y * @y)  # Uses intance variables
+  def distFromOrigin
+    Math.sqrt(@x * @x  + @y * @y) # uses instance variables directly
+  end
+  def distFromOrigin2
+    Math.sqrt(x * x + y * y) # uses getter methods
   end
 
-  def distanceFromOrigin2
-    Math.sqrt(x * x + y * y)  # Uses getters, makes 4 calls
-  end
 end
 
-class ColorPoint < Point
-  attr_accessor :color  # color, color=
+# design question: "Is a 3D-point a 2D-point?"
+class ThreeDPoint < Point
+  attr_accessor :z
 
-  def initialize(x, y, color="clear")
+  def initialize(x,y,z)
     super(x,y)
-    @color = color
+    @z = z
+  end
+  def distFromOrigin
+    d = super
+    Math.sqrt(d * d + @z * @z)
+  end
+  def distFromOrigin2
+    d = super
+    Math.sqrt(d * d + z * z)
   end
 end
+
+class PolarPoint < Point
+  # Interesting: by not calling super constructor, no x and y instance vars
+  # In Java/C#/Smalltalk would just have unused x and y fields
+  def initialize(r,theta)
+    @r = r
+    @theta = theta
+  end
+  def x
+    @r * Math.cos(@theta)
+  end
+  def y
+    @r * Math.sin(@theta)
+  end
+  def x= a
+    b = y # avoids multiple calls to y method
+    @theta = Math.atan2(b, a)
+    @r = Math.sqrt(a*a + b*b)
+    self
+  end
+  def y= b
+    a = x # avoid multiple calls to y method
+    @theta = Math.atan2(b, a)
+    @r = Math.sqrt(a*a + b*b)
+    self
+  end
+  def distFromOrigin # must override since inherited method does wrong thing
+    @r
+  end
+  # inherited distFromOrigin2 already works!! Template method pattern FTW
+end
+
+# the key example
+pp = PolarPoint.new(4,Math::PI/4)
+pp.x
+pp.y
+pp.distFromOrigin2
