16eb43f5ff34ba9a7d43085e47338114ca995137
Biryukov Egor <birykovegor@gmail.com>
2016-12-03 22:14:49 +0300

    Solve 3

--- PartC/week1/hw6assignment.rb
+++ PartC/week1/hw6assignment.rb
@@ -12,36 +12,52 @@ class MyPiece < Piece
     MyPiece.new(All_Pieces.sample, board)
   end
 
+  def self.cheat_piece (board)
+    MyPiece.new(Cheat_piece, board)
+  end
+
   # class array holding all the pieces and their rotations
+  Cheat_piece = [[[0, 0],[0, 0],[0, 0],[0, 0]]]
+
   All_Pieces = [[[[0, 0], [1, 0], [0, 1], [1, 1]]],  # square (only needs one)
-                rotations([[0, 0], [-1, 0], [1, 0], [0, -1]]), # T
-                [[[0, 0], [-1, 0], [1, 0], [2, 0]], # long (only needs two)
-                [[0, 0], [0, -1], [0, 1], [0, 2]]],
-                rotations([[0, 0], [0, -1], [0, 1], [1, 1]]), # L
-                rotations([[0, 0], [0, -1], [0, 1], [-1, 1]]), # inverted L
-                rotations([[0, 0], [-1, 0], [0, -1], [1, -1]]), # S
-                rotations([[0, 0], [1, 0], [0, -1], [-1, -1]]),
-                # Added in 2:
-                rotations([[0, 0], [0, 1], [1, 0]]),                     # 1
-                [[[0, 0], [-1, 0], [-2, 0], [1, 0], [2, 0]],             # 2
-                [[0, 0], [0, -1], [0, -2], [0, 1], [0, 2]]],
-                rotations([[0, 0], [-1, 0], [-1, -1], [1, 0], [0, -1]])] # 3
+                  rotations([[0, 0], [-1, 0], [1, 0], [0, -1]]), # T
+                  [[[0, 0], [-1, 0], [1, 0], [2, 0]], # long (only needs two)
+                    [[0, 0], [0, -1], [0, 1], [0, 2]]],
+                  rotations([[0, 0], [0, -1], [0, 1], [1, 1]]), # L
+                  rotations([[0, 0], [0, -1], [0, 1], [-1, 1]]), # inverted L
+                  rotations([[0, 0], [-1, 0], [0, -1], [1, -1]]), # S
+                  rotations([[0, 0], [1, 0], [0, -1], [-1, -1]]),  # Z
+                  # Added in 2:
+                  rotations([[0, 0], [0, 0], [0, 1], [1, 0]]),             # 1
+                  [[[0, 0], [-1, 0], [-2, 0], [1, 0], [2, 0]],             # 2
+                    [[0, 0], [0, -1], [0, -2], [0, 1], [0, 2]]],
+                  rotations([[0, 0], [-1, 0], [-1, -1], [1, 0], [0, -1]])] # 3
 end
 
 class MyBoard < Board
   def initialize (game)
     super
     @current_block = MyPiece.next_piece(self)
+    @cheat = false
   end
 
   def next_piece
-    @current_block = MyPiece.next_piece(self)
+    puts @cheat
+    @current_block = @cheat ? MyPiece.cheat_piece(self) : MyPiece.next_piece(self)
+    @cheat = false
     @current_pos = nil
   end
 
   def rotate_180
     rotate_clockwise
-    rotate_clockwise # Simple function composition
+    rotate_clockwise
+  end
+
+  def cheat
+    if @score > 100
+      @cheat = true
+      @score -= 100
+    end
   end
 end
 
@@ -57,5 +73,6 @@ class MyTetris < Tetris
   def key_bindings
     super
     @root.bind('u', proc {@board.rotate_180})
+    @root.bind('c', proc {@board.cheat})
   end
 end
