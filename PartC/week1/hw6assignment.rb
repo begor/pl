9d21d4dd76f7b67032f9f1b038411d58e3aa6354
Biryukov Egor <birykovegor@gmail.com>
2016-12-03 17:47:12 +0300

    Homework: provided files and 180 rotation

---
+++ PartC/week1/hw6assignment.rb
@@ -0,0 +1,24 @@
+class MyPiece < Piece
+end
+
+class MyBoard < Board
+  def rotate_180
+    rotate_clockwise
+    rotate_clockwise # Simple function composition
+  end
+end
+
+class MyTetris < Tetris
+  def set_board
+    @canvas = TetrisCanvas.new
+    @board = MyBoard.new(self)
+    @canvas.place(@board.block_size * @board.num_rows + 3,
+                  @board.block_size * @board.num_columns + 6, 24, 80)
+    @board.draw
+  end
+
+  def key_bindings
+    super
+    @root.bind('u', proc {@board.rotate_180})
+  end
+end
