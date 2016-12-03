ad923cd0b9598a12c0a39e727c1ab265d8b975aa
Biryukov Egor <birykovegor@gmail.com>
2016-12-03 18:23:17 +0300

    Add new figures

--- PartC/week1/hw6assignment.rb
+++ PartC/week1/hw6assignment.rb
@@ -1,7 +1,33 @@
 class MyPiece < Piece
+  def initialize (point_array, board)
+    @all_rotations = point_array
+    @rotation_index = (0..(@all_rotations.size-1)).to_a.sample
+    @color = All_Colors.sample
+    @base_position = [5, 0] # [column, row]
+    @board = board
+    @moved = true
+  end
+
+  def self.next_piece (board)
+    MyPiece.new(All_Pieces.sample, board)
+  end
+
+  # class array holding all the pieces and their rotations
+  All_Pieces = [[[[0, 0], [1, 0], [0, 1], [1, 1]]],  # square (only needs one)
+               rotations([[0, 0], [-1, 0], [-1, -1], [1, 0], [0, -1]])] # first added
 end
 
 class MyBoard < Board
+  def initialize (game)
+    super
+    @current_block = MyPiece.next_piece(self)
+  end
+
+  def next_piece
+    @current_block = MyPiece.next_piece(self)
+    @current_pos = nil
+  end
+
   def rotate_180
     rotate_clockwise
     rotate_clockwise # Simple function composition
